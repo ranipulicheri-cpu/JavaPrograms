86fafe3c5618ae127f30e014c5d1d0b8681a266b
pulis <pulis@Rani>
2026-08-04 13:00:38 +0530

    First Commit

---
+++ SR/src/Small.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Small {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+	       for(int i=1;i<=100;i++){
+	          if(i%2==0){
+	            System.out.println(i);
+	          }
+	       }
+
+
+	}
+
+}
